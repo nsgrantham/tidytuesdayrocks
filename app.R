b8d7021c32fe06630a6afd96f87d4411405bffc1
Neal Grantham <neal@nsgrantham.com>
2019-03-03 14:46:45 -0800

    Include Roboto Mono font

--- app.R
+++ app.R
@@ -11,6 +11,7 @@ tweets <- read_tsv(file.path("data", "tweets.tsv")) %>%
   filter(dataset_id != "x")
 
 ui <- fluidPage(
+  tags$head(HTML('<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">')),
   tags$head(HTML('<style>* {font-size: 100%; font-family: Roboto Mono;}</style>')),
   tags$head(HTML('<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>')),
   fluidRow(
