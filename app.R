4e333c4ce7cffaae8b27c818f02e8858db152e0c
Neal Grantham <neal@nsgrantham.com>
2020-09-14 21:04:04 -0700

    Update sidebar text and fix pagination reset

--- app.R
+++ app.R
@@ -34,6 +34,10 @@ ui <- fluidPage(
       .rt-noData {
         display: none;
       }
+      .rt-pagination-nav {
+        float: left;
+        width: 100%;
+      }
     '))
   ),
   
@@ -42,24 +46,13 @@ ui <- fluidPage(
       id = "sidebar",
       tags$h1("tidytuesday.rocks"),
       tags$br(),
-      HTML(paste("<p><a href='https://github.com/rfordatascience/tidytuesday'>Tidy Tuesday</a>",
-                 "is a weekly social data project in <a href='https://www.r-project.org/'>R</a>.",
-                 "Every week <a href='https://twitter.com/thomas_mock'>@thomas_mock</a> and",
-                 "<a href='https://twitter.com/R4DSCommunity'>@R4DSCommunity</a> post a new dataset",
-                 "and ask R users to explore it and share their findings on Twitter with",
-                 "<a href='https://twitter.com/search?src=typd&q=%23tidytuesday'>#TidyTuesday</a>.</p>")),
-      HTML(paste("<p>Since the first dataset was posted on April 2nd, 2018, there are now",
-                 "111 datasets and 4,613 #TidyTuesday tweets from 1,027 users! Use the options",
-                 "below to filter the tweets by dataset or Twitter user and sort them by date, likes, and retweets.</p>")),
-      #HTML(paste("<p>tidytuesday.rocks is about 150 lines of R code and relies on your #TidyTuesday",
-      #            "tweets, which I scrape and label every few weeks. ",
-      #            "It is built with <a href='https://shiny.rstudio.com/'>Shiny</a> and ",
-      #            "and its source code is <a href='https://github.com/nsgrantham/tidytuesdayrocks'>on GitHub</a>.</p>")),
-      # HTML(paste("<p>The response to tidytuesday.rocks has been amazing! It was even awarded ", 
-      #            "<a href='https://blog.rstudio.com/2019/04/05/first-shiny-contest-winners/'>a runner up spot in the 2019 Shiny Contest</a>",
-      #            "among 136 submissions. \U0001F57A</p>")),
-      HTML("<p> <a href='https://twitter.com/nsgrantham'>Follow me on Twitter</a> and <a href='https://github.com/nsgrantham/tidytuesdayrocks'>view the source code on GitHub</a>.</p>"),
-      tags$p("Happy plotting!"),
+      HTML(paste(
+        "<p><a href='https://github.com/rfordatascience/tidytuesday'>Tidy Tuesday</a> is a weekly data visualization challenge for R users hosted on Twitter with #TidyTuesday.</p>",
+        "<p>Since April 2nd, 2018, there are now</p>",
+        "<ul><li>4,613 tweets from</li><li>1,027 users across</li><li>111 datasets!</li></ul>",
+        "<p>Use the options below to filter the tweets by dataset or user and sort them by likes, retweets, and when they were posted. Happy plotting! \U0001F57A</p>",
+        "<p>(Made by <a href='https://twitter.com/nsgrantham'>@nsgrantham</a>. Source code <a href='https://github.com/nsgrantham/tidytuesdayrocks'>on GitHub</a>.)</p>"
+      )),
       tags$br(),
       tabsetPanel(
         id = "selected_tab",
@@ -69,37 +62,30 @@ ui <- fluidPage(
           tags$br(),
           selectInput("dataset_name", NULL, rev(datasets$dataset_name), selected = rev(datasets$dataset_name)[1]),
           selectInput("dataset_sort_by", NULL, c("Sorted by most likes", "Sorted by most retweets", "Sorted by most recent"), selected = "Sorted by most likes"),
-          tags$br(),
-          uiOutput("dataset_links") 
+          uiOutput("dataset_links")
         ),
         tabPanel("User tweets", value = "user",
           tags$br(),
           selectizeInput("user_name", NULL, users, selected = sample(users, 1)),
           selectInput("user_sort_by", NULL, c("Sorted by most likes", "Sorted by most retweets", "Sorted by most recent"), selected = "Sorted by most likes"),
-          tags$br(),
-          uiOutput("user_links") 
+          uiOutput("user_links")
         )
       ),
       width = 5
     ),
     mainPanel(
-      tags$br(),
       conditionalPanel(
         condition = "input.selected_tab == 'dataset'",
-        reactableOutput("embedded_dataset_tweets_table")
+        reactableOutput("embedded_dataset_tweets_table", width = "600px")
       ),
       conditionalPanel(
         condition = "input.selected_tab == 'user'",
-        reactableOutput("embedded_user_tweets_table")
+        reactableOutput("embedded_user_tweets_table", width = "600px")
       ),
       width = 7
     )
   )
 )
-
-embed_tweet <- function(tweet) {
-  tags$blockquote(class = "twitter-tweet", tags$a(href = tweet$status_url))
-}
 
 make_links <- function(urls, text, icon = NULL) {
   if (is.na(urls)) return("")
@@ -138,7 +124,7 @@ server <- function(input, output, session) {
   
   output$dataset_links <- renderUI({
     tagList(
-      make_links(chosen_dataset()$dataset_files, "Data", "\U0001F4BE"),        # floppy disk
+      make_links(chosen_dataset()$dataset_files, "Data Download", "\U0001F4BE"),        # floppy disk
       make_links(chosen_dataset()$dataset_articles, "Article", "\U0001F5DE"),  # rolled up newspaper
       make_links(chosen_dataset()$dataset_sources, "Source", "\U0001F4CD")     # red pin
     )    
@@ -160,13 +146,17 @@ server <- function(input, output, session) {
               src = paste0("https://twitframe.com/show?url=", url, "&datetime=", datetime),
               border = 0,
               frameborder = 0,
-              height = 600,
+              height = 625,
               width = 600
             )
           }
         )
       )
     )
+  })
+  
+  observeEvent(sorted_dataset_tweets(), {
+    updateReactable("embedded_dataset_tweets_table", page = 1)
   })
   
   user_tweets <- reactive({
@@ -190,28 +180,32 @@ server <- function(input, output, session) {
   
   output$embedded_user_tweets_table <- renderReactable({
     reactable(
-      select(sorted_user_tweets(), status_url),
+      transmute(sorted_user_tweets(), url_datetime = paste(status_url, created_at, sep = "|")),
       showPageInfo = FALSE,
       defaultPageSize = 1,
       sortable = FALSE,
       columns = list(
-        status_url = colDef(
-          cell = function(value) {
+        url_datetime = colDef(
+          cell = function(url_datetime) {
+            split_url_datetime <- unlist(str_split(url_datetime, "\\|"))
+            url <- URLencode(split_url_datetime[1], reserved = TRUE)
+            datetime <- as.numeric(as.POSIXct(split_url_datetime[2]))
             tags$iframe(
+              src = paste0("https://twitframe.com/show?url=", url, "&datetime=", datetime),
               border = 0,
               frameborder = 0,
-              height = 600,
-              width = 600,
-              src = paste0(
-                "https://twitframe.com/show?url=", str_replace_all(str_replace(value, ":", "%3A"), "/", "%2F")
-              )
+              height = 650,
+              width = 600
             )
           }
         )
       )
     )
   })
+  
+  observeEvent(sorted_user_tweets(), {
+    updateReactable("embedded_user_tweets_table", page = 1)
+  })
 }
-
 
 shinyApp(ui, server)
